8abf49ffe2abe80bc78d3ec4bb297b74c8cff7c3
Eula Bengco <eulabengco@gmail.com>
2022-02-24 16:53:06 -0500

    create component and establish routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-        <h1>HELLO</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
