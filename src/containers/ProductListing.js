b1e877e021b20348cee0ac345081eb4f546226fd
Eula Bengco <eulabengco@gmail.com>
2022-02-25 22:19:25 -0500

    create productComponent and fetch product data

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
+    const products = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const fetchProducts = async () => {
+      const response = await axios
+        .get("https://fakestoreapi.com/products")
+        .catch((err) => {
+          console.log("err", err);
+        });
+      dispatch(setProducts(response.data));
+    };
+
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+
+    console.log('products: ', products);
   return (
-    <div>
-      <h1>Product Listing</h1>
+    <div className="ui grid container" style={{marginTop: 10}}>
+      <ProductComponent />
     </div>
   );
 };
