983b2dc4b329fa3340b079f11910b88fde180ac6
Eula Bengco <eulabengco@gmail.com>
2022-02-25 22:45:32 -0500

    develop product details functionality

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProduct } from "../redux/actions/productActions";
 
 const ProductDetails = () => {
+  const product = useSelector((state) => state.product)
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+
+  const fetchProductDetail = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => {
+        console.log("err", err);
+      });
+      dispatch(selectedProduct(response.data))
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") fetchProductDetail()
+  },[productId])
+  console.log("product", product);
   return (
     <div>
       <h1>Product Component</h1>
